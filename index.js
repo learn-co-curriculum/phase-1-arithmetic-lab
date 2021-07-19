e18f6dec5fff680e9250056f457a8707b9f77051
TrevorJohnson <trevor.assetsolutions@gmail.com>
2021-07-19 14:55:56 -0600

    commit tree 6c25f7476419db933c7967d69acd28ef1b5a2db9

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 10);
+
+let num4 = 7;
+let num3 = 67;
+let mod = num3 % num4;
+
+let max = Math.max(1,2,4,5,7,9,13,16,12,20);
