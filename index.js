ac5e188744d0181c22d20aff28848debbece30d3
aleksq7 <morrisalex2001@gmail.com>
2021-08-09 12:58:01 -0600

    commit tree 267949953c4036e35ddb2548620a7226257ed99e

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const num1 = 2;
+const num2 = 31;
+const num3 = 68;
+const num4 = 8;
 
-// Write your code here
+const multiply = num1*num2;
+const random = Math.floor(Math.random())+1;
+const mod = num3%num4;
+const max = Math.max (1,4,7,10,13,20);
