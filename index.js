2362c1f4f032865518facc116d6587b5caf178d2
Caner <kurucaner@gmail.com>
2021-08-09 19:30:46 -0400

    commit tree f316b8178169013b15b850d97375e21e26e888ad

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random()*10) + 1
+
+const num3 = 11
+const num4 = 7
+const mod = num3 % num4;
+
+const max = Math.max(20)
