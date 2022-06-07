1def0ac6b02e478230aa9097431d7544bb7962e0
jemel <jemel.defreitas@gmail.com>
2022-06-07 17:42:25 -0500

    commit tree 914e456b3e0fe0d0ac03a66264cb4be2b3fa65b3

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
 // Write your code here
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2
+let random = Math.floor(Math.random() *10) + 10
+
+const num3 = 10
+const num4 = 6
+const mod = num3 % num4
+const max = Math.max(3, 15, 2, 5, 7, 20, 14, 9)
