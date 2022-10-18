ebef2300a9039ea72a429f30995c40ff7975f994
Batoul El Mansour <elmansourbatoul@gmail.com>
2022-10-18 19:05:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+const num1=31
+const num2=2
+const multiply=num1*num2
+const random=Math.floor(Math.random() * 10) + 1;
+const num3=22
+const num4=18
+const mod=num3%num4
+const max=Math.max(1, 2, 3, 4, 5,20)
