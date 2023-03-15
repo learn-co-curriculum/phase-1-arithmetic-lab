65d737422ec98d9cfd67e32f282ba1c003d29176
Guled <guletkadar@gmail.com>
2023-03-16 00:11:17 +0300

    my Work is Done

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
 
 // Write your code here
+
+// Task (1)
+
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+console.log("this is the result of Task (1) =  " +multiply);
+
+
+
+// Task (2)
+
+let rondom = Math.floor(Math.random() * 5) + 2;
+
+console.log("this is the result of Task (2) " + rondom);
+
+
+// Task (3)
+
+let num4 = 19;
+let num5 = 5;
+
+let mod = `"The reminder of this teo numbs is " ${num4 % num5}`;
+
+console.log(mod);
+
+
+// Task (4)
+
+let max = Math.max(5,10,15,20);
+console.log("The highest num of this values is " +max);
