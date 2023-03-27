144e1dba200992e3d7cc65bb21b1dc16ec4b12f2
Badr Nasher <badrnasher@gmail.com>
2023-03-28 00:25:45 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random());
+let num3 = 40;
+let num4 = 6;
+
+let mod = num3 % num4;
+
+let max = Math.max(1, 2, 3, 4, 20);
