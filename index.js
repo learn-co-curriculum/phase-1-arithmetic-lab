71522a5a91941f86d42216ae0a029b1085d35dd0
Eli Dawson <edawson@alumni.berklee.edu>
2023-04-11 12:22:56 -0400

    Completed Lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+//multiply to equal 62
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
 
-// Write your code here
+//random integer greater than 0
+let random = Math.floor(Math.random() * 10) + 1;
+
+//creates remainder of 4
+let num3 = 10;
+let num4 = 6;
+let mod = num3 % num4;
+
+//outputs highest number in set
+let max = Math.max(5, 10, 20, 15);
