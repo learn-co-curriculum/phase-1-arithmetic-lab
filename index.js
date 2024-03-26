e3a46f69a670beaa698dc8e47421c4443de7fe8d
SamieC <samchelgut@gmail.com>
2024-03-26 12:27:09 +0300

    examples added on index.js

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
 
 // Write your code here
+// Question1
+function multiply(num1, num2) {
+ const times = num1 * num2
+
+ return times
+}
+const result = multiply(31, 2)
+console.log (result)
+
+// Question2
+var randomNumber = Math.random()*100000
+const result2 = parseInt(randomNumber)
+console.log (result2)
+
+// Question3
+function mod(num3, num4){
+    const modulus = num3 % num4
+
+    return modulus
+}
+const result3 = mod(9,5)
+console.log (result3)
+
+// Question 4
+function Max(a,b,c,d,e,f){
+    const maximus = Math.max(a,b,c,d,e,f)
+
+    return maximus
+}
+
+const result4 = Max(3,7,20,9,0,14)
+console.log(result4)
